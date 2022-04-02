439489a61df002b736e5bdb9c0afa912a0d71313
StanovskyDev <stas@vdb.com.pl>
2022-04-02 14:47:50 +0200

    Solve furth excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let hashArray = new Map();
+    for (const arg of args){
+        hashArray.set(arg, true);
+    }
 
+    let newArray = new Array();
+    const arraySize = array.length;
+    for (let i = 0; i < arraySize; ++i){
+        if (hashArray.get(array[i]) != true){
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
