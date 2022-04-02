b5e459d53259e235f453545df5eb54cfacfe05c3
StanovskyDev <stas@vdb.com.pl>
2022-04-02 11:42:45 +0200

    Solve second excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, amount) {
+    let newString;
+    try{
+        newString = string.repeat(amount);
+    }
+    catch(error){
+        newString = "ERROR";
+    }
+    finally{
+        return newString;
+    }
 };
 
 // Do not edit below this line
