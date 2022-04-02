e8a571be471abbdc99abdd16a1949787ebfd17ce
StanovskyDev <stas@vdb.com.pl>
2022-04-02 15:49:41 +0200

    Solve 5th excercise in O(n)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+Array.prototype.validate = function(func){
+    for (let value of this){
+        if (!func(value)) return false;
+    }
+    return true;
+};
 
+const sumAll = function(start, end) {
+    if (!([start, end].validate(num => 0 <= num) && [start, end].validate(num => typeof(num) === "number"))){
+        return "ERROR";
+    }
+    
+    let temp = Math.min(start, end);
+    end = Math.max(start, end);
+    start = temp;
+
+    let result = 0;
+    for (let i = start; i <= end; ++i){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
