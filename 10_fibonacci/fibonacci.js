ca924c1c9098c2591fd4f892a3f467a58bb35dff
StanovskyDev <stas@vdb.com.pl>
2022-07-19 17:49:29 +0200

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS"
+    
+    let snippet = [1, 1, 2];
 
+    for (let count = 3; count < n; count++){
+        let next = snippet[1] + snippet[2];
+        snippet[0] = snippet[1];
+        snippet[1] = snippet[2];
+        snippet[2] = next;
+    }
+
+    return snippet[Math.min(n-1, 2)];
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
