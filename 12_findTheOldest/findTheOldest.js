df635b040d79a3951421c24f803863cf03417831
StanovskyDev <stas@vdb.com.pl>
2022-07-19 18:15:05 +0200

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestPerson;
+    let oldestAge = 0;
+    for (obj of arr){
+        let age;
+        if (Object.keys(obj).includes("yearOfDeath")) {
+            age = obj.yearOfDeath - obj.yearOfBirth
+        } else {
+            age = 2022 - obj.yearOfBirth;
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = obj;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
