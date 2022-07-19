b5f483f9a6a0f3c096dfc085b07444e78cf81682
StanovskyDev <stas@vdb.com.pl>
2022-07-19 17:55:34 +0200

    Solve getTheTitles with Array.prototype,map()

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,5 @@
 const getTheTitles = function(arr) {
-    let arrayOfTitles = [];
-    for (obj of arr){
-        arrayOfTitles.push(obj.title);
-    }
-    return arrayOfTitles;
+    return arr.map(obj => obj.title);
 };
 
 // Do not edit below this line
