8cdd6f48a53a6e5d3c70349a28c813ed410281f8
StanovskyDev <stas@vdb.com.pl>
2022-07-19 17:36:17 +0200

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = '';
+    string = string.toLowerCase();
+    for (let char of string){
+        if (97 <= char.charCodeAt(0) && char.charCodeAt(0) <= 122) newString += char;
+    }
 
+    console.log(newString);
+
+    sLen = newString.length;
+    for(let i = 0; i < sLen / 2 - 1; ++i){
+        if (newString[i] != newString[sLen - 1 - i]) return false
+    }
+    return true;
 };
 
 // Do not edit below this line
